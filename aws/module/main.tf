431edd3cf6463824862d868998f8e97ffb5ece14
salausss <91048429+salausss@users.noreply.github.com>
2025-01-17 06:16:00 +0000

    sadsdvdf

---
+++ aws/module/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2-test" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  security_groups = var.security_groups 
+}
