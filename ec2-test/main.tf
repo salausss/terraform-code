248602e25c32762f8750f12a4b772d8dff5356c2
salausss <91048429+salausss@users.noreply.github.com>
2025-01-17 06:38:44 +0000

    kui

--- ec2-test/main.tf
+++ ec2-test/main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "ap-south-1"
+  
 }
 
-resource "aws_instance" "ec2-test" {
-  ami = var.ami
+module "ec2-create" {
+  source = "../aws/module"
+  ami = var.ami 
   instance_type = var.instance_type
   subnet_id = var.subnet_id
   security_groups = var.security_groups
---
+++ ec2-test/variable.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+    default = "ami-00bb6a80f01f03502"
+    type = string
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "no des"
+}
+
+variable "security_groups" {
+  description = "this "
+}
