c7ef702b500e8ab0fac74198a214352c1a31521e
salausss <91048429+salausss@users.noreply.github.com>
2025-01-17 06:22:33 +0000

    mj

---
+++ aws/module/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  description = "ami for ec2"
+}
+
+variable "instance_type" {
+    description = "type for ec2"
+}
+
+variable "subnet_id" {
+  description = "subnet for ec2"
+}
+
+variable "security_groups" {
+  description = "sg for ec2"
+}
+
