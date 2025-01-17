09da8f128783f7136ff83f13395a4ef2da8b8b9f
salausss <91048429+salausss@users.noreply.github.com>
2025-01-17 04:42:16 +0000

    as

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "test_subnet1" {
 
 resource "aws_subnet" "test_subnet2" {
   vpc_id = aws_vpc.test.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
     name = "test_subnet2"
